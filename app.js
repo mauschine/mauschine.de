226d63a260c2183ea29a00dd33d3f0333513f052
Felix Klauke <info@felix-klauke.de>
2019-05-22 19:17:01 +0200

    Add helmet for security reasons.

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var hsts = require('hsts');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const hsts = require('hsts');
+const helmet = require('helmet');
 
-var indexRouter = require('./routes/index');
-var legalRouter = require('./routes/legal');
-var jokesRouter = require('./routes/jokes');
+const indexRouter = require('./routes/index');
+const legalRouter = require('./routes/legal');
+const jokesRouter = require('./routes/jokes');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,6 +21,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
+app.use(helmet.dnsPrefetchControl({ allow: true }));
 
 app.use('/', indexRouter);
 app.use('/legal', legalRouter);
